d29951353bfc2ff0b897d94f789580088801e5dd
Hachibur Rahman <hachiburrahman15@gmail.com>
2026-06-30 03:21:28 +0600

    connect react with node

---
+++ my-phone-client/src/components/Phone.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useLoaderData } from 'react-router';
+
+const Phone = () => {
+    const phone=useLoaderData()
+    console.log(phone)
+    return (
+        <div>
+            <p>{phone.name}</p>
+            <img src={phone.image} alt="" />
+        </div>
+    );
+};
+
+export default Phone;
---
+++ my-phone-client/src/components/Phones.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, useLoaderData } from 'react-router';
+
+const Phones = () => {
+    const phones=useLoaderData()
+    console.log(phones)
+    return (
+        <div>
+        {
+            phones.map(phone=><li key={phone.id}><Link to={`/phones/${phone.id}`} >{phone.name}</Link></li>)
+        }
+        </div>
+    );
+};
+
+export default Phones;
